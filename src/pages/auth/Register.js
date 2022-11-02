49be611984c146795ea6a61e413ded3126af5a25
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2022-11-02 13:25:25 +0200

    Create a register page design

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import { Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>Register</div>
+        <div className='login register'>
+            <div className='form text-center'>
+                <h3>Register</h3>
+                <form>
+
+                    <Form.Control
+                        type='text'
+                        placeholder="Email"
+                        className='formControl'
+                    />
+                    <Form.Control
+                        type='password'
+                        placeholder="Password"
+                        className='formControl'
+                    />
+                    <Form.Control
+                        type='password'
+                        placeholder="Confirm Password"
+                        className='formControl'
+                    />
+                    <div className='btn btn-primary fw-bold'>Register</div>
+                    <p className='text-capitalize notHave'>
+                        already an account?
+                        <span>
+                            <Link to='/login' className='text-decoration-none text-dark fw-bold ms-1'>log-In</Link>
+                        </span>
+                    </p>
+
+                </form>
+            </div>
+        </div>
     );
 }
 
